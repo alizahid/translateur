769d357ab942672d2ba197f8f62c48e63532e58e
Ali Zahid <ali.zahid@live.com>
2017-02-04 16:21:42 +0400

    Helper updated

--- src/helpers/translate.js
+++ src/helpers/translate.js
@@ -2,14 +2,32 @@ import {GOOGLE_TRANSLATE_API_KEY} from 'react-native-dotenv'
 
 export function request(url, language) {
 	return new Promise((resolve, reject) => {
-		fetch(url).then(response => response.json()).then(json => resolve({language: language, text: json.data.translations.pop().translatedText})).catch(reject)
+		fetch(url).then(response => response.json()).then(json => {
+			let translation = json.data.translations.pop()
+
+			resolve({language, source: translation.detectedSourceLanguage, text: translation.translatedText})
+		}).catch(reject)
 	})
 }
 
 export default function translate(text, languages) {
 	return new Promise((resolve, reject) => {
-		let requests = languages.map(language => request(`https://www.googleapis.com/language/translate/v2?q=${encodeURIComponent(text)}&target=${language}&key=${GOOGLE_TRANSLATE_API_KEY}&fields=${encodeURIComponent('translations/translatedText')}`, language))
+		let requests = languages.map(language => request(`https://www.googleapis.com/language/translate/v2?q=${encodeURIComponent(text)}&target=${language}&key=${GOOGLE_TRANSLATE_API_KEY}`, language))
 
-		Promise.all(requests).then(resolve).catch(reject)
+		Promise.all(requests).then(data => {
+			let source = data[0].source
+
+			let index = languages.indexOf(source)
+
+			if (index < 0) {
+				data.unshift({language: source, text})
+			} else if (index > 0) {
+				let main = data.splice(index, 1).pop()
+
+				data.unshift(main)
+			}
+
+			resolve(data)
+		}).catch(reject)
 	})
 }

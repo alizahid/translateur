461f3f465dac21aa80f564437389f6d970edfac5
Ali Zahid <ali.zahid@live.com>
2017-02-04 16:59:19 +0400

    Assets added

--- src/helpers/images.js
+++ src/helpers/images.js
@@ -1,9 +1,11 @@
 import check from '../assets/images/check.png'
 import left from '../assets/images/left.png'
+import menu from '../assets/images/menu.png'
 import translateur from '../assets/images/translateur.png'
 
 export default {
 	check,
 	left,
+	menu,
 	translateur
 }

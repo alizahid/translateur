49c03a222260a4b2bf62ad491eb4992e35a39778
Ali Zahid <ali.zahid@live.com>
2017-02-04 12:19:11 +0400

    Assets added

--- src/helpers/images.js
+++ src/helpers/images.js
@@ -1,7 +1,9 @@
 import check from '../assets/images/check.png'
+import left from '../assets/images/left.png'
 import translateur from '../assets/images/translateur.png'
 
 export default {
 	check,
+	left,
 	translateur
 }

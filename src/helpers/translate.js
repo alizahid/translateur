6cd6b53073863524cb0bc74c830057a2ddc6db86
Ali Zahid <ali.zahid@live.com>
2017-02-04 12:19:24 +0400

    Refactoring

--- src/helpers/translate.js
+++ src/helpers/translate.js
@@ -2,15 +2,13 @@ import {GOOGLE_TRANSLATE_API_KEY} from 'react-native-dotenv'
 
 export function request(url, language) {
 	return new Promise((resolve, reject) => {
-		fetch(url).then(response => response.json().then(json => resolve({language: language, text: json.data.translations.pop().translatedText}))).catch(reject)
+		fetch(url).then(response => response.json()).then(json => resolve({language: language, text: json.data.translations.pop().translatedText})).catch(reject)
 	})
 }
 
 export default function translate(text, languages) {
 	return new Promise((resolve, reject) => {
-		let fields = encodeURIComponent('translations/translatedText')
-
-		let requests = languages.map(language => request(`https://www.googleapis.com/language/translate/v2?q=${text}&target=${language.code}&key=${GOOGLE_TRANSLATE_API_KEY}&fields=${fields}`, language))
+		let requests = languages.map(language => request(`https://www.googleapis.com/language/translate/v2?q=${encodeURIComponent(text)}&target=${language}&key=${GOOGLE_TRANSLATE_API_KEY}&fields=${encodeURIComponent('translations/translatedText')}`, language))
 
 		Promise.all(requests).then(resolve).catch(reject)
 	})
